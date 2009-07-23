6a0ca4d7bff86ce3457baefa5a001e8d7eeb3974
Alex MacCaw <maccman@gmail.com>
2009-07-23 11:28:05 +0100

    show card id\

--- public/javascripts/taskboard.js
+++ public/javascripts/taskboard.js
@@ -245,7 +245,7 @@ TASKBOARD.builder.buildCardFromJSON = function(card){
 	if(card.issue_no){
 		cardLi += $.tag('span', $.tag('a', card.issue_no, { href : card.url, rel : 'external'}) + ": ",	{ className : 'alias' });
 	}
-	cardLi += $.tag("span", card.name.escapeHTML(), { className : 'title' });
+	cardLi += $.tag("span", '#' + card.id + ' ' + card.name.escapeHTML(), { className : 'title' });
 
   cardLi += $.tag("span", "points: " + $.tag("span", card.points, { className : 'points' }), { className : 'progress' });
 
