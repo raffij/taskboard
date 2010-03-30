5f7526e72357f084c7e6e023e7ce33f8d4e4765c
James Higgs <higgis@therealhiggis-664.local>
2010-03-30 18:30:11 +0100

    Added a migration to give all users access to all existing taskboards

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100330111227) do
+ActiveRecord::Schema.define(:version => 20100330172426) do
 
   create_table "cards", :force => true do |t|
     t.text     "name",                                       :null => false
